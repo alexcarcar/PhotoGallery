a7888736c0661946d59a4ffc0aab1ebb4b9e31b9
acarcar <alexcarcar@yahoo.com>
2020-06-24 01:26:06 -0700

    Chapter 24: Challenge - Paging

--- app/src/main/java/alex/carcar/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/alex/carcar/photogallery/PhotoGalleryFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -12,10 +13,12 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 private const val TAG = "PhotoGalleryFragment"
+private const val PAGE_SIZE = 25
 
 class PhotoGalleryFragment : Fragment() {
     private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
     private lateinit var photoRecyclerView: RecyclerView
+    private lateinit var pageBar: SeekBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +33,23 @@ class PhotoGalleryFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
         photoRecyclerView = view.findViewById(R.id.photo_recycler_view)
         photoRecyclerView.layoutManager = GridLayoutManager(context, 3)
+        pageBar = view.findViewById(R.id.pageBar)
+        pageBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                photoGalleryViewModel.progress = progress
+                photoGalleryViewModel.galleryItemLiveData.removeObservers(viewLifecycleOwner)
+                photoRecyclerView.adapter = null
+                photoRecyclerView.adapter = PhotoAdapter(
+                    photoGalleryViewModel.galleryItemLiveData.value!!,
+                    photoGalleryViewModel.progress * PAGE_SIZE
+                )
+                pageBar.max = photoGalleryViewModel.galleryItemLiveData.value!!.size / PAGE_SIZE - 1
+                pageBar.progress = photoGalleryViewModel.progress
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
         return view
     }
 
@@ -38,16 +58,23 @@ class PhotoGalleryFragment : Fragment() {
         photoGalleryViewModel.galleryItemLiveData.observe(
             viewLifecycleOwner,
             Observer { galleryItems ->
-                photoRecyclerView.adapter = PhotoAdapter(galleryItems)
+                photoRecyclerView.adapter =
+                    PhotoAdapter(galleryItems, photoGalleryViewModel.progress * PAGE_SIZE)
+                pageBar.max = galleryItems.size / PAGE_SIZE - 1
+                pageBar.progress = photoGalleryViewModel.progress
             }
         )
+
     }
 
     private class PhotoHolder(itemTextView: TextView) : RecyclerView.ViewHolder(itemTextView) {
         val bindTitle: (CharSequence) -> Unit = itemTextView::setText
     }
 
-    private class PhotoAdapter(private val galleryItems: List<GalleryItem>) :
+    private class PhotoAdapter(
+        private val galleryItems: List<GalleryItem>,
+        val offset: Int
+    ) :
         RecyclerView.Adapter<PhotoHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoHolder {
             val textView = TextView(parent.context)
@@ -57,7 +84,8 @@ class PhotoGalleryFragment : Fragment() {
         override fun getItemCount(): Int = galleryItems.size
 
         override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
-            val galleryItem = galleryItems[position]
+            if (offset + position >= galleryItems.size) return
+            val galleryItem = galleryItems[offset + position]
             holder.bindTitle(galleryItem.title)
         }
 
--- app/src/main/java/alex/carcar/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/alex/carcar/photogallery/PhotoGalleryViewModel.kt
@@ -4,9 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 
 class PhotoGalleryViewModel : ViewModel() {
+    var progress: Int
     val galleryItemLiveData: LiveData<List<GalleryItem>>
 
     init {
         galleryItemLiveData = FlickrFetchr().fetchPhotos()
+        progress = 0
     }
 }
