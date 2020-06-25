424855b6a4b7ebf64f33ab3407d5fb87d74dd6b5
acarcar <alexcarcar@yahoo.com>
2020-06-24 17:45:26 -0700

    Chapter 25: Loopers, Handlers, and HandlerThread

--- app/src/main/java/alex/carcar/photogallery/FlickrFetchr.kt
+++ app/src/main/java/alex/carcar/photogallery/FlickrFetchr.kt
@@ -3,9 +3,13 @@ package alex.carcar.photogallery
 import alex.carcar.photogallery.api.FlickrApi
 import alex.carcar.photogallery.api.FlickrResponse
 import alex.carcar.photogallery.api.PhotoResponse
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.Log
+import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -50,5 +54,13 @@ class FlickrFetchr {
 
         })
         return responseLiveData
+    }
+
+    @WorkerThread
+    fun fetchPhoto(url: String): Bitmap? {
+        val response: Response<ResponseBody> = flickrApi.fetchUrlBytes(url).execute()
+        val bitmap = response.body()?.byteStream()?.use(BitmapFactory::decodeStream)
+        Log.i(TAG, "Decoded bitmap=$bitmap from Response=$response")
+        return bitmap
     }
 }
