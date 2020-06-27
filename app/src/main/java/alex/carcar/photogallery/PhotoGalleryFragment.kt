7f2d7e7094695775b4ee3044bda4422539593de7
acarcar <alexcarcar@yahoo.com>
2020-06-27 10:56:52 -0700

    Chapter 29: Browsing the Web and WebView

--- app/src/main/java/alex/carcar/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/alex/carcar/photogallery/PhotoGalleryFragment.kt
@@ -138,8 +138,25 @@ class PhotoGalleryFragment : VisibleFragment() {
         }
     }
 
-    private class PhotoHolder(itemImageView: ImageView) : RecyclerView.ViewHolder(itemImageView) {
+    private inner class PhotoHolder(itemImageView: ImageView) :
+        RecyclerView.ViewHolder(itemImageView), View.OnClickListener {
+
+        private lateinit var galleryItem: GalleryItem
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
         val bindDrawable: (Drawable) -> Unit = itemImageView::setImageDrawable
+
+        fun bindGalleryItem(item: GalleryItem) {
+            galleryItem = item
+        }
+
+        override fun onClick(view: View) {
+            val intent = PhotoPageActivity.newIntent(requireContext(), galleryItem.photoPageUri)
+            startActivity(intent)
+        }
     }
 
     private inner class PhotoAdapter(private val galleryItems: List<GalleryItem>) :
@@ -154,6 +171,7 @@ class PhotoGalleryFragment : VisibleFragment() {
 
         override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
             val galleryItem = galleryItems[position]
+            holder.bindGalleryItem(galleryItem)
             val placeholder: Drawable =
                 ContextCompat.getDrawable(requireContext(), R.drawable.ic_launcher_foreground)
                     ?: ColorDrawable()
