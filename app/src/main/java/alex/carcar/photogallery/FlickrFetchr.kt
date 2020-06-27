c3f837a9ef0187eea6195195ab446cf9cb0cefd5
acarcar <alexcarcar@yahoo.com>
2020-06-26 17:19:52 -0700

    Chapter 26: SearchView and SharedPreferences

--- app/src/main/java/alex/carcar/photogallery/FlickrFetchr.kt
+++ app/src/main/java/alex/carcar/photogallery/FlickrFetchr.kt
@@ -2,6 +2,7 @@ package alex.carcar.photogallery
 
 import alex.carcar.photogallery.api.FlickrApi
 import alex.carcar.photogallery.api.FlickrResponse
+import alex.carcar.photogallery.api.PhotoInterceptor
 import alex.carcar.photogallery.api.PhotoResponse
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -9,6 +10,7 @@ import android.util.Log
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import okhttp3.OkHttpClient
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
@@ -22,16 +24,29 @@ class FlickrFetchr {
     private val flickrApi: FlickrApi
 
     init {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(PhotoInterceptor())
+            .build()
+
         val retrofit: Retrofit = Retrofit.Builder()
             .baseUrl("https://api.flickr.com/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
+
         flickrApi = retrofit.create(FlickrApi::class.java)
     }
 
     fun fetchPhotos(): LiveData<List<GalleryItem>> {
+        return fetchPhotosMetaData(flickrApi.fetchPhotos())
+    }
+
+    fun searchPhotos(query: String): LiveData<List<GalleryItem>> {
+        return fetchPhotosMetaData(flickrApi.searchPhotos(query))
+    }
+
+    private fun fetchPhotosMetaData(flickrRequest: Call<FlickrResponse>): LiveData<List<GalleryItem>> {
         val responseLiveData: MutableLiveData<List<GalleryItem>> = MutableLiveData()
-        val flickrRequest: Call<FlickrResponse> = flickrApi.fetchPhotos()
 
         flickrRequest.enqueue(object : Callback<FlickrResponse> {
             override fun onFailure(call: Call<FlickrResponse>, t: Throwable) {
